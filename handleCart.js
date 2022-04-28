93d9d1b138610913adc64500719b65c1ba79841d
munizsaj <munizsaj@dukes.jmu.edu>
2022-04-28 19:52:22 -0400

    Sockets are now handled by connections.js

--- handleCart.js
+++ handleCart.js
@@ -5,6 +5,27 @@ global.CARTSPEED = 0
 const { cartOutgoingEvents, localIncomingEvents, localOutgoingEvents } = require('./connections')
 const cartState = require('./cartState')
 
+// // module.exports.handle = (io) => {
+// //   localOutgoingEvents.map((x) => {
+// //     eventManager.on(x, (data) => {
+// //         io.of('/online').emit(x, data)
+// //     })
+// // })
+
+//   // io.of('/online').on('connection', socket => {
+//     localIncomingEvents.map((x) => {
+//         socket.on(x, (data) => eventManager.emit(x, data))
+//     })
+
+//     socket.on('cart-connect', async (data) => {
+//       cart.socket = socket
+//       await cartState.connect(data)
+//       eventManager.emit('active-change', true)
+//     })
+
+//   // })
+// }
+
 module.exports.isConnected = () => {
   if (cart.socket) {
     return cart.socket.connected
@@ -14,19 +35,10 @@ module.exports.isConnected = () => {
 }
 
 module.exports.handle = async (nsp) => {
-  // cartOutgoingEvents.map((x) => {
-  //   eventManager.on(x, (data) => {
-  //     if (cart.socket) cart.socket.emit(x, data)
-  //   })
-  // })
   localOutgoingEvents.map((x) => {
     eventManager.on(x, (data) => {
       nsp.emit(x, data)
     })
-  })
-
-  eventManager.on('get-destinations', (x) => {
-    console.log(x)
   })
 
   nsp.on('connection', (socket) => {
@@ -34,67 +46,96 @@ module.exports.handle = async (nsp) => {
       socket.on(x, (data) => eventManager.emit(x, data))
     })
 
-    socket.on('cart-connect', async (data) => {
-      cart.socket = socket
-      await cartState.connect(data)
-      eventManager.emit('active-change', true)
-    })
-
-    socket.on('summon-finish', () => {
-      cartState.summonFinish()
-    })
-
-    socket.on('gps', (data) => {
-      CARTGPS.latitude = data.latitude
-      CARTGPS.longitude = data.longitude
-      eventManager.emit('cart-gps', data)
-    })
-
-    socket.on('speed', (data) => {
-      CARTSPEED = data
-      eventManager.emit('cart-speed', data)
-    })
-
-
-    socket.on('logs', (data) => {
-      eventManager.emit('logs', data)
-    })
-
-    socket.on('destination', (data) => {
-      eventManager.emit('destination-change', data)
-    })
-
-    socket.on('tts', (data) => {
-      eventManager.emit('tts', data)
-    })
-
-    socket.on('pullover', (data) => {
-      console.log("cloud receiving pullover :" + data)
-      eventManager.emit('change-pullover', data)
-    })
-
-    socket.on('path', (data) => {
-      CARTSTATE().path = data
-      eventManager.emit('path', data)
-    })
-
-    socket.on('cart-active', (data) => {
-      data === true ? cartState.reconnect() : cartState.disconnect()
-    })
-
-    socket.on('transit-start', (data) => cartState.transitStart(data))
-
-    socket.on('transit-end', async () => await cartState.transitEnd())
-
-    socket.on('passenger-exit', async () => await cartState.passengerExit())
-
-    socket.on(
-      'destination',
-      async (name) => await cartState.setDestination(name)
-    )
-
-    socket.on('disconnect', async () => {
-      await cartState.disconnect()
+    socket.on('change-destination', (x) => {
+      console.log("Changed Destination: " + x)
     })
   })
+
+  
 }
+
+// module.exports.handle = async (nsp) => {
+//   // cartOutgoingEvents.map((x) => {
+//   //   eventManager.on(x, (data) => {
+//   //     if (cart.socket) cart.socket.emit(x, data)
+//   //   })
+//   // })
+//   localOutgoingEvents.map((x) => {
+//     eventManager.on(x, (data) => {
+//       nsp.emit(x, data)
+//     })
+//   })
+
+//   eventManager.on('get-destinations', (x) => {
+//     console.log(x)
+//   })
+
+//   nsp.on('connection', (socket) => {
+//     localIncomingEvents.map((x) => {
+//       socket.on(x, (data) => eventManager.emit(x, data))
+//     })
+
+//     socket.on('cart-connect', async (data) => {
+//       cart.socket = socket
+//       await cartState.connect(data)
+//       eventManager.emit('active-change', true)
+//     })
+
+//     socket.on('summon-finish', () => {
+//       cartState.summonFinish()
+//     })
+
+//     socket.on('gps', (data) => {
+//       CARTGPS.latitude = data.latitude
+//       CARTGPS.longitude = data.longitude
+//       eventManager.emit('cart-gps', data)
+//     })
+
+//     socket.on('speed', (data) => {
+//       CARTSPEED = data
+//       eventManager.emit('cart-speed', data)
+//     })
+
+
+//     socket.on('logs', (data) => {
+//       eventManager.emit('logs', data)
+//     })
+
+//     socket.on('destination', (data) => {
+//       eventManager.emit('destination-change', data)
+//     })
+
+//     socket.on('tts', (data) => {
+//       eventManager.emit('tts', data)
+//     })
+
+//     socket.on('pullover', (data) => {
+//       console.log("cloud receiving pullover :" + data)
+//       eventManager.emit('change-pullover', data)
+//     })
+
+//     socket.on('path', (data) => {
+//       CARTSTATE().path = data
+//       eventManager.emit('path', data)
+//     })
+
+//     socket.on('cart-active', (data) => {
+//       data === true ? cartState.reconnect() : cartState.disconnect()
+//     })
+
+//     socket.on('transit-start', (data) => cartState.transitStart(data))
+
+//     socket.on('transit-end', async () => await cartState.transitEnd())
+
+//     socket.on('passenger-exit', async () => await cartState.passengerExit())
+
+//     socket.on(
+//       'destination',
+//       async (name) => await cartState.setDestination(name)
+//     )
+
+//     socket.on('disconnect', async () => {
+//       await cartState.disconnect()
+//     })
+//   })
+// }
